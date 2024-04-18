26e508fb039c75b7eb95d2a83158e3fbf61dcf15
SAMIUL2022 <sami35bd@gmail.com>
2024-04-18 20:43:02 +0600

    .

---
+++ Day_6/A_Absolute_Maximization.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+
+{
+  int t;
+  cin>>t;
+  while (t--)
+  {
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+       cin>>v[i];
+    }
+    int ans=v[0];int c=v[0];
+for (int i = 1; i < n; i++)
+{
+    ans&=v[i];
+    c|=v[i];
+}
+cout<<c-ans<<endl;
+  }
+  
+
+
+
+    return 0;
+}
